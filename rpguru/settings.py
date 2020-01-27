052a9353cedacf8a1b50dfb3259209bc02e52314
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2020-01-27 13:54:50 +0100

    Rewritten settings to use one file and load secrets from ini-like .env

--- rpguru/settings.py
+++ rpguru/settings.py
@@ -1,15 +1,21 @@
-"""RPGuru Base Settings File"""
+"""RPGuru Settings file"""
 
 import os
+import configparser
+import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILE_DIR = os.environ.get('HOME')
 
 # Load secrets from JSON file
-import json
-secrets = json.loads(open(os.path.join(os.path.dirname(__file__), 'secrets.json')).read())
+secrets = configparser.ConfigParser()
+secrets.read(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = secrets['secret_key']
-ALLOWED_HOSTS = ['.rpguru.com']
+DEBUG = secrets.getboolean('DEFAULT', 'debug', fallback=False)
+SECRET_KEY = secrets['DEFAULT']['secret_key']
+if 'allowed_hosts' not in secrets['DEFAULT']:
+    ALLOWED_HOSTS = ['.rpguru.com']
+else:
+    ALLOWED_HOSTS = json.loads(secrets.get('DEFAULT', 'allowed_hosts'))
 SITE_ID = 1
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
@@ -17,8 +23,11 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-DEFAULT_FROM_EMAIL = 'webmaster@rpguru.com'
-SERVER_EMAIL = 'root@rpguru.com'
+if 'email' in secrets:
+    if 'default_from_email' in secrets['email']:
+        DEFAULT_FROM_EMAIL = secrets.get('email', 'default_from_email')
+    if 'server_email' in secrets['email']:
+        SERVER_EMAIL = secrets.get('email', 'server_email')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -83,12 +92,22 @@ DATABASES = {
         'HOST': '',  # unix socket
         'NAME': 'rpguru',
         'USER': 'rpguru',
-        'PASSWORD': secrets['database_password'],
+        'PASSWORD': secrets['database']['password'],
         'TEST': {
             'NAME': 'rpguru_test'
         }
     }
 }
+
+if 'email' in secrets:
+    if 'host' in secrets['email']:
+        EMAIL_HOST = secrets.get('email', 'host')
+    if 'host_user' in secrets['email']:
+        EMAIL_HOST_USER = secrets.get('email', 'host_user')
+    if 'host_user' in secrets['email']:
+        EMAIL_HOST_PASSWORD = secrets.get('email', 'host_password')
+    if 'port' in secrets['email']:
+        EMAIL_PORT = secrets.get('email', 'port')
 
 AUTH_USER_MODEL = 'core.RPGuruUser'
 AUTHENTICATION_BACKENDS = (
@@ -99,7 +118,10 @@ LOGIN_URL = '/account/login'
 LOGIN_REDIRECT_URL = '/account/profile'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+if 'auth' in secrets and 'account_email_verification' in secrets['auth']:
+    ACCOUNT_EMAIL_VERIFICATION = secrets.get('auth', 'account_email_verification')
+else:
+    ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_MIN_LENGTH = 3
@@ -127,7 +149,10 @@ PASSWORD_HASHERS = [
 
 # Log file must be writable by Django server process
 from django.utils.log import DEFAULT_LOGGING
-LOGFILE = os.path.join(FILE_DIR, 'log', 'rpguru.log')
+if 'logfile' in secrets['DEFAULT']:
+    LOGFILE = secrets.get('DEFAULT', 'logfile')
+else:
+    LOGFILE = os.path.join(FILE_DIR, 'log', 'rpguru.log')
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -191,6 +216,22 @@ LOGGING = {
         'django.server': DEFAULT_LOGGING['loggers']['django.server'],
     }
 }
+
+# Sentry
+if 'sentry' in secrets and secrets.getboolean('sentry', 'enable', fallback=True):
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+    sentry_sdk.init(
+        dsn=secrets['sentry']['dsn'],
+        integrations=[DjangoIntegration()],
+        send_default_pii=True
+    )
+
+# Django Debug Toolbar
+if 'debug-toolbar' in secrets and secrets.getboolean('debug-toolbar', 'enable', fallback=False):
+    INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+    INTERNAL_IPS = ('127.0.0.1',)
 
 # Date format
 from django.conf.locale.en import formats as en_formats
