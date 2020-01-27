9f207bd60aa495259e8b912ad343026462ae4e81
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2020-01-27 17:40:23 +0100

    Further settings file tweaks

--- rpguru/settings.py
+++ rpguru/settings.py
@@ -6,16 +6,16 @@ import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILE_DIR = os.environ.get('HOME')
 
-# Load secrets from JSON file
-secrets = configparser.ConfigParser()
-secrets.read(os.path.join(BASE_DIR, '.env'))
+# Load configuration from INI-like .env file
+config = configparser.ConfigParser()
+config.read(os.path.join(BASE_DIR, '.env'))
 
-DEBUG = secrets.getboolean('DEFAULT', 'debug', fallback=False)
-SECRET_KEY = secrets['DEFAULT']['secret_key']
-if 'allowed_hosts' not in secrets['DEFAULT']:
+DEBUG = config.getboolean('DEFAULT', 'debug', fallback=False)
+SECRET_KEY = config['DEFAULT']['secret_key']
+if 'allowed_hosts' not in config['DEFAULT']:
     ALLOWED_HOSTS = ['.rpguru.com']
 else:
-    ALLOWED_HOSTS = json.loads(secrets.get('DEFAULT', 'allowed_hosts'))
+    ALLOWED_HOSTS = json.loads(config.get('DEFAULT', 'allowed_hosts'))
 SITE_ID = 1
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
@@ -23,11 +23,11 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-if 'email' in secrets:
-    if 'default_from_email' in secrets['email']:
-        DEFAULT_FROM_EMAIL = secrets.get('email', 'default_from_email')
-    if 'server_email' in secrets['email']:
-        SERVER_EMAIL = secrets.get('email', 'server_email')
+if 'email' in config:
+    if 'default_from_email' in config['email']:
+        DEFAULT_FROM_EMAIL = config.get('email', 'default_from_email')
+    if 'server_email' in config['email']:
+        SERVER_EMAIL = config.get('email', 'server_email')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -92,22 +92,22 @@ DATABASES = {
         'HOST': '',  # unix socket
         'NAME': 'rpguru',
         'USER': 'rpguru',
-        'PASSWORD': secrets['database']['password'],
+        'PASSWORD': config['database']['password'],
         'TEST': {
             'NAME': 'rpguru_test'
         }
     }
 }
 
-if 'email' in secrets:
-    if 'host' in secrets['email']:
-        EMAIL_HOST = secrets.get('email', 'host')
-    if 'host_user' in secrets['email']:
-        EMAIL_HOST_USER = secrets.get('email', 'host_user')
-    if 'host_user' in secrets['email']:
-        EMAIL_HOST_PASSWORD = secrets.get('email', 'host_password')
-    if 'port' in secrets['email']:
-        EMAIL_PORT = secrets.get('email', 'port')
+if 'email' in config:
+    if 'host' in config['email']:
+        EMAIL_HOST = config.get('email', 'host')
+    if 'host_user' in config['email']:
+        EMAIL_HOST_USER = config.get('email', 'host_user')
+    if 'host_user' in config['email']:
+        EMAIL_HOST_PASSWORD = config.get('email', 'host_password')
+    if 'port' in config['email']:
+        EMAIL_PORT = config.get('email', 'port')
 
 AUTH_USER_MODEL = 'core.RPGuruUser'
 AUTHENTICATION_BACKENDS = (
@@ -118,8 +118,8 @@ LOGIN_URL = '/account/login'
 LOGIN_REDIRECT_URL = '/account/profile'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-if 'auth' in secrets and 'account_email_verification' in secrets['auth']:
-    ACCOUNT_EMAIL_VERIFICATION = secrets.get('auth', 'account_email_verification')
+if 'auth' in config and 'account_email_verification' in config['auth']:
+    ACCOUNT_EMAIL_VERIFICATION = config.get('auth', 'account_email_verification')
 else:
     ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
@@ -149,8 +149,8 @@ PASSWORD_HASHERS = [
 
 # Log file must be writable by Django server process
 from django.utils.log import DEFAULT_LOGGING
-if 'logfile' in secrets['DEFAULT']:
-    LOGFILE = secrets.get('DEFAULT', 'logfile')
+if 'logfile' in config['DEFAULT']:
+    LOGFILE = config.get('DEFAULT', 'logfile')
 else:
     LOGFILE = os.path.join(FILE_DIR, 'log', 'rpguru.log')
 LOGGING = {
@@ -218,17 +218,17 @@ LOGGING = {
 }
 
 # Sentry
-if 'sentry' in secrets and secrets.getboolean('sentry', 'enable', fallback=True):
+if 'sentry' in config and config.getboolean('sentry', 'enable', fallback=True):
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
     sentry_sdk.init(
-        dsn=secrets['sentry']['dsn'],
+        dsn=config['sentry']['dsn'],
         integrations=[DjangoIntegration()],
         send_default_pii=True
     )
 
 # Django Debug Toolbar
-if 'debug-toolbar' in secrets and secrets.getboolean('debug-toolbar', 'enable', fallback=False):
+if 'debug-toolbar' in config and config.getboolean('debug-toolbar', 'enable', fallback=False):
     INSTALLED_APPS += ('debug_toolbar',)
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
     INTERNAL_IPS = ('127.0.0.1',)
